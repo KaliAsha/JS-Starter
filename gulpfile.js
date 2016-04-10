ccf7be8bb71adfd3f2a679fd763e73f2cbd17437
KaliAsha <kaliasha.kali@gmail.com>
2016-04-10 16:48:46 +0200

    Add SASS compilation

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,22 @@ var $ = require('gulp-load-plugins')()
 var browserSync = require('browser-sync')
 var reload = browserSync.reload
 
-var root = path.resolve(__dirname)
+var root = path.resolve(__dirname, 'FrontEnd/')
+var browser_support = ['last 2 versions']
 
 var src = {
   server: path.resolve(root, './'),
   css: path.resolve(root, 'css/'),
+  scss: path.resolve(root, 'css/scss/'),
   img: path.resolve(root, 'img/'),
   js: path.resolve(root, 'js/'),
   refresh: [path.resolve(root, '**/*.html'), path.resolve(root, 'css/*.css'), path.resolve(root, 'js/*.js')]
 }
 
-
-gulp.task('default', function () {
+/**
+ * Default gulp's task, start BrowserSync
+ */
+gulp.task('default', ['sass'], function () {
   var opts = {
     notify: false,
     open: false
@@ -28,5 +32,21 @@ gulp.task('default', function () {
     }
   }
   browserSync(opts)
+  $.watch(src.scss + '/**/*.scss', function () {
+    return gulp.start('sass')
+  })
   return $.watch(src.refresh).on('change', reload)
+})
+
+/**
+ * Compile .scss files and add vendor prefixes
+ */
+gulp.task('sass', function () {
+  return gulp.src(src.scss + '/**/*.scss')
+  .pipe($.plumber())
+  .pipe($.sass().on('error', $.sass.logError))
+  .pipe($.autoprefixer({
+    browsers: browser_support
+  }))
+  .pipe(gulp.dest(src.css))
 })
